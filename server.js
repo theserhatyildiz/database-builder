552584f434037f2259aa4dcdaa23c8bfbad3c2ca
Serhat Yildiz <yildizserhat@yahoo.com>
2023-10-16 15:17:11 +0300

    declared variables and connected to mongodb

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 const PORT = 8005
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'database-builder-api',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log(`Connected to Database`)
+        db = client.db(dbName)
+        collection = db.collection('builder-api')
+    })
